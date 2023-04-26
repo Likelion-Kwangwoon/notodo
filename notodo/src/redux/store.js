5c619d09205de380f1cfa95ecefc6ac32090a2ab
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2023-04-27 02:02:16 +0900

    로그인 토큰 만료 진행중

--- notodo/src/redux/store.js
+++ notodo/src/redux/store.js
@@ -15,8 +15,12 @@ import listReducer from "./slice/listSlice"
 export const logIn = createAction("LOGIN");
 export const logOut = createAction("LOGOUT");
 
-const initialState = { token: "" };
+// const initialState = { token: "" };
 
+const initialState = {
+  token : "",
+  expirationTime  : "",
+}
 const persistConfig = {
   key: "root",
   storage,
@@ -25,9 +29,12 @@ const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(logIn, (state, action) => {
       state.token = action.payload
+      state.expirationTime = new Date(new Date().getTime() + 60 * 1 * 60)
+      console.log(state.expirationTime)
     })
     .addCase(logOut, (state) => {
       state.token = ""
+      state.expirationTime = ""
     })
 })
 
