da4a0c71d458d7dae9aceb546266b313fe0d9c6d
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-05-13 19:58:17 +0900

    Feat: 설정 페이지 마크업

--- notodo/src/pages/SettingPage/index.jsx
+++ notodo/src/pages/SettingPage/index.jsx
@@ -1,7 +1,11 @@
-import * as S from './style';
+import { Ul, Li } from './style';
 
 export default function SettingPage() {
   return (
-    <></>
+    <Ul>
+      <Li onClick={()=>{}}>이용약관</Li>
+      <Li onClick={()=>{}}>로그아웃</Li>
+      <Li onClick={()=>{}}>계정 탈퇴</Li>
+    </Ul>
   )
 }
