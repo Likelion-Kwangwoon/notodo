3dc40aa6ac1d95eadef1320763545ba226497e06
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2023-05-25 22:54:16 +0900

    Feat: logout

--- notodo/src/pages/SettingPage/index.jsx
+++ notodo/src/pages/SettingPage/index.jsx
@@ -1,10 +1,19 @@
 import { Ul, Li } from './style'
-
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import { logOut } from '../../redux/store'
 export default function SettingPage() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    sessionStorage.removeItem('token');
+    dispatch(logOut());
+    navigate("/");
+  };
   return (
     <Ul>
       <Li onClick={()=>{}}>이용약관</Li>
-      <Li onClick={()=>{}}>로그아웃</Li>
+      <Li onClick={handleLogout}>로그아웃</Li>
       <Li onClick={()=>{}}>계정 탈퇴</Li>
     </Ul>
   )
