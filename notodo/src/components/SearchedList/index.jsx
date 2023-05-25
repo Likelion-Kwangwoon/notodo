5899e62fde49feab91a0e766705de50bc77f67cd
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2023-05-25 23:07:48 +0900

    fix : 나를 검색 했을 때 나는 오류 해결
    나를 검색해도 아무것도 안뜸 이제

--- notodo/src/components/SearchedList/index.jsx
+++ notodo/src/components/SearchedList/index.jsx
@@ -8,20 +8,12 @@ export default function SearchedList(props) {
   return(
     <S.ListWrap>
       {
-        !props.result.email
+        !(props.result && props.result.email) 
           ?
           <div><p>찾으려는 이메일을 검색해보세요</p></div>
           :
           <S.List>
-          {
-            // searchedData.map((user, index) => 
-            //   <div key={index}>
-            //     <SearchedListComp user={user}/>
-            //   </div>
-            // )
-            
             <SearchedListComp user={props.result} />
-          }
           </S.List>
       }
     </S.ListWrap>
