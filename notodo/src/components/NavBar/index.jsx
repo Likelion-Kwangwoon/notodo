537cc4d288005de16870d0a1f5399c52e24b280f
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-05-31 22:09:53 +0900

    Feat: navBar 종류별 url 아이콘 활성화 구현

--- notodo/src/components/NavBar/index.jsx
+++ notodo/src/components/NavBar/index.jsx
@@ -8,38 +8,49 @@ import iconHeart from "../../assets/icon-heart.svg"
 import iconHeartFill from "../../assets/icon-heart-fill.svg"
 import iconProfile from "../../assets/icon-profile.svg"
 import iconProfileFill from "../../assets/icon-profile-fill.svg"
+import { useEffect, useState } from 'react'
 
 export default function NavBar() {
   const navigate = useNavigate()
   const location = useLocation()
+  const [savelocation, setSaveLocation] = useState('')
+  const isNavLoc = (
+    location.pathname === "/mycalendar" || 
+    location.pathname === "/search" ||
+    location.pathname === "/follow" ||
+    location.pathname === "/setting"
+    )
+  
+  useEffect(() => {
+    if(isNavLoc)
+      setSaveLocation(location.pathname)
+  }, [location.pathname])
   
   return (
     <S.NavWrap>
       <S.NavUl>
         <S.NavLi
-          className={location.pathname.includes('/my') || 
-          location.pathname.includes('/notodo') ? 'on' : 'off'}
+          className={savelocation === "/mycalendar" ? 'on' : 'off'}
           onClick={() => navigate('/mycalendar')}>
-            <img src={location.pathname.includes('/my') || 
-            location.pathname.includes('/notodo') ? iconHomeFill : iconHome} />
+            <img src={savelocation === "/mycalendar" ? iconHomeFill : iconHome} />
             <p>홈</p>
         </S.NavLi>
         <S.NavLi
-          className={location.pathname.includes('/search') ? 'on' : 'off'}
+          className={savelocation === "/search" ? 'on' : 'off'}
           onClick={() => navigate('/search')}>
-            <img src={location.pathname.includes("/search") ? iconSearchFill : iconSearch} />
+            <img src={savelocation === "/search" ? iconSearchFill : iconSearch} />
             <p>검색</p>
         </S.NavLi>
         <S.NavLi
-          className={location.pathname.includes('/follow') ? 'on' : 'off'}
+          className={savelocation === "/follow" ? 'on' : 'off'}
           onClick={() => navigate('/follow')}>
-            <img src={location.pathname.includes("/follow") ? iconHeartFill : iconHeart} />
+            <img src={savelocation === "/follow" ? iconHeartFill : iconHeart} />
             <p>친구</p>
         </S.NavLi>
         <S.NavLi
-          className={location.pathname.includes('/setting') ? 'on' : 'off'}
+          className={savelocation === "/setting" ? 'on' : 'off'}
           onClick={() => navigate('/setting')}>
-            <img src={location.pathname.includes("/setting") ? iconProfileFill : iconProfile} />
+            <img src={savelocation === "/setting" ? iconProfileFill : iconProfile} />
             <p>설정</p>
         </S.NavLi>
       </S.NavUl>
